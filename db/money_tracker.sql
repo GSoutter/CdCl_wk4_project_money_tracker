19528fd031c052550a4a40ad60bdb47d85f1c7af
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-03 15:41:20 +0100

    adds database setup sql and Sql_runner

---
+++ db/money_tracker.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS merchants;
+
+CREATE TABLE merchants
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR
+);
+
+CREATE TABLE tags
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    active BOOLEAN,
+    img_url TEXT
+);
+
+CREATE TABLE transactions
+(
+    id SERIAL PRIMARY KEY,
+    transcation_timestamp TIMESTAMP,
+    amount DECIMAL,
+    merchant_id INT references merchants(id),
+    tag_id INT references tags(id)
+);
