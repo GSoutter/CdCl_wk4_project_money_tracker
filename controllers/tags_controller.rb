38ea4d52bc43fe4f1608808c234275a1bdfa8125
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-05 10:13:34 +0100

    adds tags index page

---
+++ controllers/tags_controller.rb
@@ -0,0 +1,15 @@
+require('sinatra')
+require('sinatra/contrib')
+require( 'pry-byebug' )
+
+
+require_relative( '../models/merchant.rb' )
+require_relative( "../models/tag.rb" )
+require_relative( "../models/transaction.rb" )
+also_reload('../models/*')
+
+
+get '/tags' do
+  @tags = Tag.all
+  erb(:"tags/index")
+end
